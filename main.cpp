1eaf45747e3c4ad0f6c6894d917b7704bbb83fab
Manar-tarek <36464813+Manar-tarek@users.noreply.github.com>
2018-03-27 05:09:32 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float fehrenheit ,celsius;
+  cout<<"enter celsius";
+  cin>>celsius;
+  fehrenheit=(9*celsius)/5+32;
+  cout<<"fehrenheit="<<fehrenheit;
+    return 0;
+}
